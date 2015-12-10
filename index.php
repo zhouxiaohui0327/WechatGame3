e3a568f7387ce15000e4a9ee9928b91556b60fb0
zhouxiaohui0327 <475503798@qq.com>
2015-12-10 11:24:22 +0800

    modify12.10.1

--- index.php
+++ index.php
@@ -29,19 +29,49 @@
            $(".chief_right").animate({right:"-300px"},2000);
        }
     </script>
+    <style>
+        @media screen and (min-width: 320px){.huochai{top:340px;}}
+        @media screen and (min-width: 360px){.huochai{top:387px;}}
+        @media screen and (min-width: 375px){.huochai{top:404px;}}
+        @media screen and (min-width: 384px){.huochai{top:415px;}}
+        @media screen and (min-width: 414px){.huochai{top:450px;}}
 
+            .huochai img{
+                margin-top: 30px;margin-right: 30px;
+                animation:myfirst 2s infinite linear;
+                -webkit-animation:myfirst 2s infinite linear;  Safari and Chrome
+            }
+            @keyframes myfirst
+            {
+                0% {margin-top: 30px;margin-right: 30px}
+                50% {margin-top: 54px;margin-right: -10px}
+                100%{margin-top:30px;margin-right: 30px;}
+            }
+            @-webkit-keyframes myfirst  Safari and Chrome
+            {
+                0% {margin-top: 30px;margin-right: 30px}
+                50% {margin-top: 55px;margin-right: -10px}
+                100%{margin-top:30px;margin-right: 30px;}
+            }
+
+
+    </style>
 </head>
 <body onload="setTimeout('myfunction()',2000)">
 <div id="d-mask" style="display:none"></div>                   <!--黑色透明背景-->
 <div class="wrap">
-    <div class="chief chief_left">
-        <img  src="./images/1212.png" alt=""/>
-    </div>
-    <div class="chief chief_right">
-        <img  src="./images/123123.png" alt=""/>
-    </div>
+<!--    <div class="chief chief_left">-->
+<!--        <img  src="./images/1212.png" alt=""/>-->
+<!--    </div>-->
+<!--    <div class="chief chief_right">-->
+<!--        <img  src="./images/123123.png" alt=""/>-->
+<!--    </div>-->
     <div class="container">
-        <img class="bg-pic" src="./images/bg-pic.jpg" alt=""/>
+        <img class="bg-pic" src="./images/bg-pic111.jpg" alt=""/>
+        <div class="huochai" style="position: absolute;;width: 100%;text-align: right">
+            <img style="width: 23%;margin-top: 30px;margin-right: 30px;" src="images/huochai1.png" alt=""/>
+        </div>
+
         <p class="skill">技术支持 : 安徽瀚滔网络</p>
     </div>
     <div class="thr_btn">
